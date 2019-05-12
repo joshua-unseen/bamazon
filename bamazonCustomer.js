d061a04cddd4a24d5ddd4918e1e38c68fa3955bf
joshua-unseen <corpselight@gmail.com>
2019-05-11 18:28:03 -0700

    DB connection, first query roughed in.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,27 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+var dbConnection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Gilgamesh",
+    database: "bamazon_db"
+});
+
+dbConnection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    mainPrompt();
+    dbConnection.end();
+});
+
+function mainPrompt() {
+    dbConnection.query("select product_name from products", function (err, results) {})
+    inquirer.prompt([
+        {
+            message: "What do you want to buy today?",
+            type: "list",
+
+        }
+    ])
+}
