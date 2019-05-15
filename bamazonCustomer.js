83f84697f7149054b1eab40499720f5277501736
joshua-unseen <corpselight@gmail.com>
2019-05-14 21:50:44 -0700

    Logic is pretty solid now.  Comments.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,7 @@ const keys = require("./keys.js");
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
-
+// create the DB connection
 const dbConnection = mysql.createConnection({
     host: "localhost",
     user: keys.sql.user,
@@ -11,13 +11,15 @@ const dbConnection = mysql.createConnection({
     database: "bamazon_db"
 });
 
+//Connect to the DB
 dbConnection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + dbConnection.threadId + "\n");
     readDB();
-    dbConnection.end();
+    // dbConnection.end();
 });
 
+// Get the basic product information for the interactive prompt
 function readDB() {
     var resultsArr = [];
     dbConnection.query("select item_id, product_name, price from products", function (err, results) {
@@ -33,6 +35,7 @@ function readDB() {
     });
 }
 
+// The main prompt.  Shows items for sale, and an exit option
 function mainPrompt(resultsArr) {
     inquirer.prompt([
         {
@@ -45,15 +48,22 @@ function mainPrompt(resultsArr) {
                 resultsArr.forEach(item => {
                     theArray.push(item.join(" "));
                 });
+                theArray.push("Exit");
                 return theArray;
             },
             name: "item"
         }
     ]).then(answer => {
-        console.log(resultsArr);
-        console.log(answer.item.split(" ")[0]);
+        // console.log(resultsArr);
+        // console.log(answer.item);
+        // console.log(answer.item.split(" ")[0]);
+        if (answer.item === "Exit") {
+            dbConnection.end();
+            return;
+        }
         var itemNum = parseInt(answer.item.split(" ")[0]);
         var theItem = []
+        // Search on item number, 'cuz the array will not track with item_num if products are deleted from the DB
         resultsArr.forEach(el => {
             // console.log(el);
             if (el[0] === itemNum) {
@@ -61,10 +71,11 @@ function mainPrompt(resultsArr) {
                 theItem = el;
             }
         });
-        console.log(theItem);
+        // console.log(theItem);
+        // Get the quantity desired.
         inquirer.prompt([
             {
-                message: "How many " + theItem[1] + "s would you like?",
+                message: "How many " + theItem[1] + " items would you like?",
                 type: "number",
                 name: "quantity",
                 validate: function (input) {
@@ -75,14 +86,53 @@ function mainPrompt(resultsArr) {
                 }
             }
         ]).then(answer => {
-            console.log(answer.quantity);
+            // console.log(answer.quantity);
             stockUpdate(itemNum, answer.quantity);
         }).catch(errorHandler);
     }).catch(errorHandler);
 }
 
+// Handle stock quantity verification and updating the DB
 function stockUpdate(key, quantity) {
-    
+    dbConnection.query(
+        "select item_id, stock_quantity, price from products where ?",
+        {
+            "item_id": key
+        },
+        function (err, results) {
+            if (err) {
+                console.log(err);
+            }
+            var inStock = results[0].stock_quantity;
+            var itemPrice = results[0].price;
+            // console.log(inStock);
+            // console.log(itemPrice);
+            if (inStock < quantity) {
+                console.log("Sorry, insufficient stock on hand.\n");
+                readDB();
+            }
+            else {
+                var yourCost = itemPrice * quantity;
+                console.log("We can fulfill that order.  Your cost is $" + yourCost + "\n");
+                dbConnection.query(
+                    "update products set stock_quantity = ? where ?",
+                    [
+                        inStock - quantity,
+                        {
+                            "item_id": results[0].item_id
+                        }
+                    ],
+                    function(err, results,fields) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        // console.log(results);
+                        // console.log(fields);
+                        readDB();
+                    }
+                );
+            }
+        });
 }
 
 function errorHandler(err) {
